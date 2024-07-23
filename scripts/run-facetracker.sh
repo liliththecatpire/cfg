471efb027468d3dbbd96bc4dc136f11a009cc543
nvkomata <linuko@proton.me>
2024-07-23 23:30:34 +0000

    e

--- scripts/run-facetracker.sh
+++ scripts/run-facetracker.sh
@@ -1,5 +1,5 @@
 #!/bin/fish
 
-cd ~/Documents/Vtube/OpenSeeFace/
+cd ~/OpenSeeFace/
 source venv/bin/activate.fish
 python facetracker.py -c 0 --discard-after 0 --scan-every 0 --no-3d-adapt 1 --max-feature-updates 900 --model 4

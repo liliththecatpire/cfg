5e9f95ed7e8531fc8da9caabb8b74b964e3ca66d
nvkomata <linuko@proton.me>
2024-09-02 13:17:14 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,3 +5,10 @@ end
 alias hx="helix"
 alias webptopng="magick mogrify -format png *.webp && rm -rf *.webp"
 alias eza="eza --icons"
+
+switch (uname)
+    case Linux
+        bind \es "fish_commandline_prepend run0"
+    case '*'
+        bind \es "fish_commandline_prepend sudo"
+end

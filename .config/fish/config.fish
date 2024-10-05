c2dd1b96cfd2b92a3f3cb9883a98cbd0e52f2954
nvkomata <linuko@tutamail.com>
2024-10-05 11:41:07 +0200

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,13 +2,13 @@ if status is-interactive
     # im fishing
 end
 
-alias hx="helix"
 alias webptopng="magick mogrify -format png *.webp && rm -rf *.webp"
 alias eza="eza --icons"
 
 switch (uname)
     case Linux
         bind \es "fish_commandline_prepend run0"
+        alias hx="helix"
     case '*'
         bind \es "fish_commandline_prepend sudo"
 end

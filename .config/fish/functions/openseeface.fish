fa9c52fea68f148c0255ffd39e470a2f24e73bf7
lilith <linuko@tutamail.com>
2025-01-29 19:13:08 +0100

    Delete .config/fish/functions/openseeface.fish

--- .config/fish/functions/openseeface.fish
+++
@@ -1,5 +0,0 @@
-function openseeface
-    cd ~/osf_track
-    ./venv/bin/python facetracker.py -c 0 --discard-after 0 --scan-every 0 --no-3d-adapt 1 --max-feature-updates 900 --model 4
-    cd --
-end
